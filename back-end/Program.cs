166b401a6222a2220dd1d6227a85cf351989de1e
gercel29121992 <106564023+gercel29121992@users.noreply.github.com>
2022-08-30 08:33:49 -0400

    Update Program.cs

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -63,20 +63,5 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseCors();
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-
-app.MapControllers();
 
 app.Run();
